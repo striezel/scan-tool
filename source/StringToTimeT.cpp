7e427589984633bd826572e29f664521951e1fe4
Dirk Stolle <striezel-dev@web.de>
2015-12-05 19:49:29 +0100

    add time conversion function

---
+++ source/StringToTimeT.cpp
@@ -0,0 +1,77 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of scan-tool.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include "StringToTimeT.hpp"
+#include <ctime>
+#include "../libthoro/common/StringUtils.h"
+
+bool stringToTimeT(const std::string& dateStr, std::time_t& tp)
+{
+  //date format in string should be "YYYY-MM-DD HH:mm:ss"
+  unsigned int year = 0;
+  if (!stringToUnsignedInt(dateStr.substr(0,4), year))
+    return false;
+  unsigned int month = 0;
+  if (!stringToUnsignedInt(dateStr.substr(5,2), month))
+    return false;
+  if ((month > 12) or (month < 1))
+    return false;
+  unsigned int day = 0;
+  if (!stringToUnsignedInt(dateStr.substr(8,2), day))
+    return false;
+  if ((day > 31) or (day < 1))
+    return false;
+  unsigned int hour = 25;
+  if (!stringToUnsignedInt(dateStr.substr(11,2), hour))
+    return false;
+  if (hour > 23)
+    return false;
+  unsigned int minute = 61;
+  if (!stringToUnsignedInt(dateStr.substr(14,2), minute))
+    return false;
+  if (minute > 59)
+    return false;
+  unsigned int second = 61;
+  if (!stringToUnsignedInt(dateStr.substr(17,2), second))
+    return false;
+  if (second > 59)
+    return false;
+
+  struct tm theTM;
+  theTM.tm_year = year - 1900; //years since 1900
+  theTM.tm_mon = month - 1; //months since January
+  theTM.tm_mday = day; //day of the month
+  theTM.tm_hour = hour;
+  theTM.tm_min = minute;
+  theTM.tm_sec = second;
+  //wday and yday are not known
+  theTM.tm_yday = 0;
+  theTM.tm_wday = 0;
+  //DST information is not available
+  theTM.tm_isdst = -1;
+
+  const std::time_t t = std::mktime(&theTM);
+  //Did conversion fail?
+  if (t == -1)
+    return false;
+
+  tp = t;
+  return true;
+}
---
+++ test/date/string-to-time-t/main.cpp
@@ -0,0 +1,85 @@
+/*
+ -------------------------------------------------------------------------------
+    This file is part of the test suite for scan-tool.
+    Copyright (C) 2015  Thoronador
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ -------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include <vector>
+#include "../../../source/StringToTimeT.hpp"
+
+const int secsInHour = 3600;
+const int secsInDay  = 86400;
+
+const std::vector<std::string> testCases =
+    {
+        {"1970-01-01 00:00:00"},
+        {"1970-01-01 00:00:01"},
+        {"1970-01-01 00:00:02"},
+        {"1970-01-01 00:01:00"},
+        {"1970-01-01 00:02:00"},
+        {"1970-01-01 01:00:00"},
+        {"1970-01-01 02:00:00"},
+        {"1970-01-02 00:00:00"},
+        {"1970-02-01 00:00:00"},
+        {"1970-03-31 01:10:55"},
+        {"1970-04-30 02:20:05"},
+        {"1970-05-31 03:30:10"},
+        {"1970-06-30 04:40:17"},
+        {"1970-07-31 05:50:32"},
+        {"1970-08-31 06:05:47"},
+        {"1970-09-30 07:15:50"},
+        {"1970-10-31 08:25:55"},
+        {"1970-11-30 09:35:57"},
+        {"1970-12-31 10:45:54"},
+        {"2000-02-29 23:59:59"},
+        {"2015-01-01 12:34:56"},
+        {"2015-12-31 23:01:07"},
+        {"2030-11-22 00:11:22"},
+    };
+
+
+
+int main()
+{
+  for(const auto& item : testCases)
+  {
+    std::time_t tt;
+    const bool success = stringToTimeT(item, tt);
+    if (!success)
+    {
+      std::cout << "Error: Could not convert " << item << " to std::time_t!" << std::endl;
+      return 1;
+    } //if
+
+    std::tm * pointer_tm = std::localtime(&tt);
+    char buffer[32];
+    std::strftime(buffer, 32, "%Y-%m-%d %H:%M:%S", pointer_tm);
+    const std::string result(buffer);
+
+    if (result != item)
+    {
+      std::cout << "Error: time_t for " << item << " converts to " << result
+                << "!" << std::endl;
+      return 1;
+    }
+  } //for
+
+  //Everything seems to be OK.
+  std::cout << "Test for time_t conversion was passed!" << std::endl;
+  return 0;
+}
